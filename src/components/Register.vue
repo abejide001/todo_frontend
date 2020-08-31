abc1f0f5b9c43dbfc0833d7dd3c45d0f37481e40
abejide001 <abejidefemi1@gmail.com>
2020-08-31 14:38:37 +0000

    setup registration page

---
+++ src/components/Register.vue
@@ -0,0 +1,48 @@
+<!-- eslint disable -->
+ <template>
+  <div>
+    <h1>Register</h1>
+    <input type="email" name="email" v-model="email" placeholder="email" />
+    <br />
+    <input type="name" name="name" v-model="name" placeholder="name" />
+    <br />
+    <input type="password" name="password" v-model="password" placeholder="password" />
+    <input
+      type="password"
+      name="passwordConfirm"
+      v-model="passwordConfirm"
+      placeholder="passwordConfirm"
+    />
+    <br />
+    <button @click="register">Register</button>
+  </div>
+</template>
+
+<script>
+import AuthenticationService from "../services/AuthenticationService"
+
+export default {
+  data() {
+    return {
+      email: "",
+      name: "",
+      password: "",
+      passwordConfirm: ""
+    };
+  },
+  methods: {
+      async register () {
+          const response = await AuthenticationService.register({
+              email: this.email,
+              name: this.name,
+              password: this.password,
+              passwordConfirm: this.passwordConfirm
+          })
+      }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Register from '@/components/Register'
 
 Vue.use(Router)
 
@@ -9,7 +11,12 @@ export default new Router({
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
-    }
+      component: HelloWorld,
+    },
+    {
+      path: '/register',
+      name: 'Register',
+      component: Register,
+    },
   ]
 })
